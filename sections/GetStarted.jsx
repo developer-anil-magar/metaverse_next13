59a3aa40d4a33539b785b242d47c2408b6f6c15b
developer-anil-magar <anilmagar.developer@gmail.com>
2023-04-05 11:08:46 +0545

    metaverse

---
+++ sections/GetStarted.jsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+import { TypingText, StartSteps, TitleText } from '../components';
+import styles from '../styles';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { startingFeatures } from '../constants';
+
+const GetStarted = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: 'false', amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    >
+      <motion.div
+        variants={planetVariants('left')}
+        className={`flex-1 ${styles.flexCenter}`}
+      >
+        <img
+          src="/get-started.png"
+          alt="get-started"
+          className="w-[90%] h-[90%] object-contain"
+        />
+      </motion.div>
+    </motion.div>
+  </section>
+);
+
+export default GetStarted;
